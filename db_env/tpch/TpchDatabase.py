5abfbfa5205e01d835b780b87b765980826b235d
Michał Matczak <michalmatczak22@gmail.com>
2021-11-19 00:08:45 +0100

    Minor refactor in TpchDatabase.py

--- db_env/tpch/TpchDatabase.py
+++ db_env/tpch/TpchDatabase.py
@@ -2,7 +2,6 @@ from typing import Dict
 
 from db_env.Database import Database
 from db_env.tpch.TpchBenchmark import TpchBenchmark
-from db_env.tpch.TpchGenerator import TpchGenerator
 from shared_utils.utils import get_connection, create_logger
 from db_env.tpch.config import TPCH_MUTABLE_COLUMNS
 
@@ -16,16 +15,14 @@ class TpchDatabase(Database):
         connection, cursor = get_connection(self._log, True)
 
         table_name, key_name, action_name = self.action_mapper[action]
-        index_name = f'{table_name}_{key_name}_index'
 
-        self._log.info(f"Executing action - {action_name} '{index_name}'")
+        self._log.info(f"Executing action - {action_name} '{self._get_index_name(table_name, key_name)}'")
         if action_name == "DROP INDEX":
-            sql = f'DROP INDEX {index_name} ON {table_name}'
+            self._drop_index(cursor, table_name, key_name)
+            self._state[table_name][key_name] = False
         else:
-            sql = f'CREATE INDEX {index_name} ON {table_name} ({key_name})'
-
-        cursor.execute(sql)
-        self._state[table_name][key_name] = action_name != "DROP INDEX"
+            self._create_index(cursor, table_name, key_name)
+            self._state[table_name][key_name] = True
 
         cursor.close()
         connection.close()
@@ -39,18 +36,13 @@ class TpchDatabase(Database):
         self._log.info('Resetting indexes')
 
         for table_name, key_name in TPCH_MUTABLE_COLUMNS:
-            index_name = f'{table_name}_{key_name}_index'
-
-            if self._index_exist(cursor, table_name, index_name):
-                self._log.info(f"Dropping index '{index_name}'")
-                sql = f'DROP INDEX {index_name} ON {table_name}'
-                cursor.execute(sql)
-            connection.commit()
+            if self._index_exist(cursor, table_name, key_name):
+                self._drop_index(cursor, table_name, key_name)
+                connection.commit()
+            self._state[table_name][key_name] = False
 
         cursor.close()
         connection.close()
-
-        self._state = self._get_current_mapped_database()
 
     def _get_current_mapped_database(self) -> Dict[str, Dict[str, bool]]:
         connection, cursor = get_connection(self._log, True)
@@ -59,16 +51,34 @@ class TpchDatabase(Database):
 
         state = {table_name: dict() for table_name, _ in TPCH_MUTABLE_COLUMNS}
         for table_name, key_name in TPCH_MUTABLE_COLUMNS:
-            index_name = f'{table_name}_{key_name}_index'
-            state[table_name][key_name] = self._index_exist(cursor, table_name, index_name)
+            state[table_name][key_name] = self._index_exist(cursor, table_name, key_name)
 
         cursor.close()
         connection.close()
 
         return state
 
-    def _index_exist(self, cursor, table_name: str, index_name: str) -> bool:
+    def _get_index_name(sefl, table_name, key_name):
+        return f'{table_name}_{key_name}_index'
+
+    def _index_exist(self, cursor, table_name: str, key_name: str) -> bool:
+        index_name = self._get_index_name(table_name, key_name)
+
         sql = f"SHOW INDEX FROM {table_name} WHERE KEY_NAME = '{index_name}'"
         cursor.execute(sql)
 
         return len(cursor.fetchall()) > 0
+
+    def _drop_index(self, cursor, table_name: str, key_name: str):
+        index_name = self._get_index_name(table_name, key_name)
+
+        self._log.info(f"Dropping index '{index_name}'")
+        sql = f'DROP INDEX {index_name} ON {table_name}'
+        cursor.execute(sql)
+
+    def _create_index(self, cursor, table_name: str, key_name: str):
+        index_name = self._get_index_name(table_name, key_name)
+
+        self._log.info(f"Creating index '{index_name}'")
+        sql = f'CREATE INDEX {index_name} ON {table_name} ({key_name})'
+        cursor.execute(sql)
